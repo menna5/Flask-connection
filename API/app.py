ab22453af9316c252a5a372e467828fd2272ad28
Menna <Mena08800@gmail.com>
2023-12-04 16:53:45 +0200

    yarab yng7

--- API/app.py
+++ API/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return '15'
 
 if __name__ == '__main__':
-    app.run(host='192.168.1.9', port=5000)
+    app.run(host='192.168.1.7', port=5000)
